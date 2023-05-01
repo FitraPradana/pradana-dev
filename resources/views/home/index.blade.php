2f20969a97a71c72645f548b235860f7e1c59864
FitraPradana <pradanafitrah45@gmail.com>
2023-05-01 07:03:06 +0700

    v2.1

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -53,44 +53,98 @@
                 <ul>
                     <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
                     {{-- <li><a class="nav-link scrollto" href="#team">Team</a></li> --}}
-                    <li><a class="nav-link scrollto" href="#cv">Curiculum Vitae</a></li>
-                    <li><a class="nav-link   scrollto" href="#portfolio">Portfolio</a></li>
-                    <li class="dropdown"><a href="#"><span>Template</span> <i class="bi bi-chevron-down"></i></a>
+                    <li class="dropdown"><a href="#template"><span>Template</span> <i
+                                class="bi bi-chevron-down"></i></a>
                         <ul>
-                            <li class="dropdown"><a href="#"><span>Website</span> <i
+                            <li class="dropdown"><a href="#"><span>Template Admin</span> <i
                                         class="bi bi-chevron-right"></i></a>
                                 <ul>
-                                    <li class="dropdown"><a href="#"><span>Template Admin</span> <i
+                                    <li><a href="https://pradana-live.com/template/website/Admin_AceAdmin(v1.4)"
+                                            target="_blank">Ace Admin (v1.4)</a>
+                                    </li>
+                                    <li><a href="https://pradana-live.com/template/website/Admin_AdminBSBMaterialDesignV1.0.7"
+                                            target="_blank">Admin BSB Material Design (v1.0.7)</a>
+                                    </li>
+                                    <li><a href="https://pradana-live.com/template/website/Admin_AdminLTE-1.3.1"
+                                            target="_blank">Admin LTE (v1.3.1)</a>
+                                    </li>
+                                    <li><a href="https://pradana-live.com/template/website/Admin_AdminLTE-2.4.18"
+                                            target="_blank">Admin LTE (v2.4.18)</a>
+                                    </li>
+                                    <li><a href="https://pradana-live.com/template/website/Admin_AdminLTE-3.2.0"
+                                            target="_blank">Admin LTE (v3.2.0)</a>
+                                    </li>
+                                    <li><a href="https://pradana-live.com/template/website/Admin_Dompet"
+                                            target="_blank">Dompet</a>
+                                    </li>
+                                    <li><a href="https://pradana-live.com/template/website/Admin_DreamAdmin"
+                                            target="_blank">Dream Admin</a>
+                                    </li>
+                                    <li><a href="https://pradana-live.com/template/website/Admin_Edumin"
+                                            target="_blank">Edumin</a>
+                                    </li>
+                                    <li><a href="https://pradana-live.com/template/website/Admin_FocusAdmin"
+                                            target="_blank">Focus Admin</a>
+                                    </li>
+                                    <li><a href="https://pradana-live.com/template/website/Admin_Gentelella-1.4.0/production"
+                                            target="_blank">Gentelella (v1.4.0)</a>
+                                    </li>
+                                    <li><a href="https://pradana-live.com/template/website/Admin_Mazer"
+                                            target="_blank">Mazer</a>
+                                    </li>
+                                    <li><a href="https://pradana-live.com/template/website/Admin_Muzik"
+                                            target="_blank">Muzik</a>
+                                    </li>
+                                    <li><a href="https://pradana-live.com/template/website/Admin_PickAdmin"
+                                            target="_blank">Pick Admin</a>
+                                    </li>
+                                    <li><a href="https://pradana-live.com/template/website/Admin_PurpleAdmin"
+                                            target="_blank">Purple Admin</a>
+                                    </li>
+                                    <li><a href="https://pradana-live.com/template/website/Admin_Quixlab"
+                                            target="_blank">Quixlab</a>
+                                    </li>
+                                    <li><a href="https://pradana-live.com/template/website/Admin_SBAdmin-1.0.1"
+                                            target="_blank">SB Admin (v1.0.1)</a>
+                                    </li>
+                                    <li><a href="https://pradana-live.com/template/website/Admin_Skodash"
+                                            target="_blank">Skodash</a>
+                                    </li>
+                                    <li><a href="https://pradana-live.com/template/website/Admin_Viho/admin"
+                                            target="_blank">Viho</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="dropdown"><a href="#"><span>E-Commerce</span> <i
+                                        class="bi bi-chevron-right"></i></a>
+                                <ul>
+                                    <li class="dropdown"><a href="#"><span>Website</span> <i
                                                 class="bi bi-chevron-right"></i></a>
                                         <ul>
-                                            <li><a href="https://pradana-live.com/template/website/Admin_AceAdmin(v1.4)"
-                                                    target="_blank">Ace Admin (v1.4)</a>
-                                            </li>
-                                            <li><a href="https://pradana-live.com/template/website/Admin_Dompet"
-                                                    target="_blank">Dompet</a>
-                                            </li>
-                                            <li><a href="https://pradana-live.com/template/website/Admin_FocusAdmin"
-                                                    target="_blank">Focus Admin</a>
-                                            </li>
-                                            <li><a href="https://pradana-live.com/template/website/Admin_Muzik"
-                                                    target="_blank">Muzik</a>
-                                            </li>
-                                            <li><a href="https://pradana-live.com/template/website/Admin_PickAdmin"
-                                                    target="_blank">Pick Admin</a>
-                                            </li>
-                                            <li><a href="https://pradana-live.com/template/website/Admin_Skodash"
-                                                    target="_blank">Skodash</a>
-                                            </li>
+                                            {{-- <li><a href="https://pradana-live.com/template/website/Ecommerce_DreamPos"
+                                                    target="_blank">Dream Pos</a></li> --}}
                                         </ul>
                                     </li>
-                                    <li class="dropdown"><a href="#"><span>E-Commerce</span> <i
+                                    <li class="dropdown"><a href="#"><span>Admin Dashboard</span> <i
                                                 class="bi bi-chevron-right"></i></a>
                                         <ul>
                                             <li><a href="https://pradana-live.com/template/website/Ecommerce_DreamPos"
                                                     target="_blank">Dream Pos</a></li>
                                         </ul>
                                     </li>
-                                    <li class="dropdown"><a href="#"><span>School Management</span> <i
+                                </ul>
+                            </li>
+                            <li class="dropdown"><a href="#"><span>School Management</span> <i
+                                        class="bi bi-chevron-right"></i></a>
+                                <ul>
+                                    <li class="dropdown"><a href="#"><span>Website</span> <i
+                                                class="bi bi-chevron-right"></i></a>
+                                        <ul>
+                                            {{-- <li><a href="https://pradana-live.com/template/website/Ecommerce_DreamPos"
+                                                    target="_blank">Dream Pos</a></li> --}}
+                                        </ul>
+                                    </li>
+                                    <li class="dropdown"><a href="#"><span>Admin Dashboard</span> <i
                                                 class="bi bi-chevron-right"></i></a>
                                         <ul>
                                             <li><a href="https://pradana-live.com/template/website/SchoolManagementAdmin_PreSchoolV1"
@@ -99,14 +153,17 @@
                                                     target="_blank">Pre School v2</a></li>
                                         </ul>
                                     </li>
-                                    {{-- <li><a href="#">Deep Drop Down 2</a></li> --}}
                                 </ul>
                             </li>
+                            {{-- <li><a href="#">Deep Drop Down 2</a></li> --}}
                             {{-- <li><a href="#">Drop Down 2</a></li>
                             <li><a href="#">Drop Down 3</a></li>
                             <li><a href="#">Drop Down 4</a></li> --}}
                         </ul>
                     </li>
+                    <li><a class="nav-link scrollto" href="#cv">Curiculum Vitae</a></li>
+                    <li><a class="nav-link   scrollto" href="#portfolio">Portfolio</a></li>
+
                     <li class="dropdown"><a href="#"><span>Pages</span> <i class="bi bi-chevron-down"></i></a>
                         <ul>
                             <li><a class="nav-link scrollto" href="#about">About</a></li>
@@ -274,6 +331,151 @@
             </div>
         </section> --}}
         <!-- End Team Section -->
+
+        <!-- ======= TEMPLATE Section ======= -->
+        <section id="template" class="portfolio">
+            <div class="container" data-aos="fade-up">
+
+                <div class="section-title">
+                    <h2>TEMPLATE</h2>
+                    <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit
+                        sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias
+                        ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+                </div>
+
+                <ul id="portfolio-flters" class="d-flex justify-content-center" data-aos="fade-up"
+                    data-aos-delay="100">
+                    <li data-filter="*" class="filter-active">All</li>
+                    <li data-filter=".filter-website">Website</li>
+                    <li data-filter=".filter-admin">Admin Dashboard</li>
+                </ul>
+
+                <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
+
+
+                    <!-- WEBSITE -->
+                    <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+                        <div class="portfolio-img"><img
+                                src="{{ asset('/') }}source_Arsha/assets/img/portfolio/sistem_overtime/Beranda.png"
+                                class="img-fluid" alt=""></div>
+                        <div class="portfolio-info">
+                            <h4>Dashboard</h4>
+                            <p>Sistem Overtime</p>
+                            <a href="{{ asset('/') }}source_Arsha/assets/img/portfolio/sistem_overtime/Beranda.png"
+                                data-gallery="portfolioGallery" class="portfolio-lightbox preview-link"
+                                title="Dashboard"><i class="bx bx-plus"></i></a>
+                            <a href="{{ asset('/') }}source_Arsha/assets/img/portfolio/sistem_overtime/Login.png"
+                                data-gallery="portfolioGallery" class="portfolio-lightbox preview-link"
+                                title="Login"></a>
+                            <a href="{{ asset('/') }}source_Arsha/assets/img/portfolio/sistem_overtime/Master User.png"
+                                data-gallery="portfolioGallery" class="portfolio-lightbox preview-link"
+                                title="Master User"></a>
+                            <a href="{{ asset('/') }}source_Arsha/assets/img/portfolio/sistem_overtime/Master Department.png"
+                                data-gallery="portfolioGallery" class="portfolio-lightbox preview-link"
+                                title="Master Department"></a>
+                            <a href="{{ asset('/') }}source_Arsha/assets/img/portfolio/sistem_overtime/Transaksi.png"
+                                data-gallery="portfolioGallery" class="portfolio-lightbox preview-link"
+                                title="Transaksi"></a>
+                            <a href="{{ asset('/') }}source_Arsha/assets/img/portfolio/sistem_overtime/Laporan.png"
+                                data-gallery="portfolioGallery" class="portfolio-lightbox preview-link"
+                                title="Laporan"></a>
+                            {{-- <a href="portfolio-details.html" class="details-link" title="More Details"><i
+                                    class="bx bx-link"></i></a> --}}
+                        </div>
+                    </div>
+
+                    {{-- <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+                        <div class="portfolio-img"><img
+                                src="{{ asset('/') }}source_Arsha/assets/img/portfolio/portfolio-3.jpg"
+                                class="img-fluid" alt=""></div>
+                        <div class="portfolio-info">
+                            <h4>App 2</h4>
+                            <p>App</p>
+                            <a href="{{ asset('/') }}source_Arsha/assets/img/portfolio/portfolio-3.jpg"
+                                data-gallery="portfolioGallery" class="portfolio-lightbox preview-link"
+                                title="App 2"><i class="bx bx-plus"></i></a>
+                            <a href="portfolio-details.html" class="details-link" title="More Details"><i
+                                    class="bx bx-link"></i></a>
+                        </div>
+                    </div> --}}
+
+                    {{-- <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+                        <div class="portfolio-img"><img
+                                src="{{ asset('/') }}source_Arsha/assets/img/portfolio/portfolio-6.jpg"
+                                class="img-fluid" alt=""></div>
+                        <div class="portfolio-info">
+                            <h4>App 3</h4>
+                            <p>App</p>
+                            <a href="{{ asset('/') }}source_Arsha/assets/img/portfolio/portfolio-6.jpg"
+                                data-gallery="portfolioGallery" class="portfolio-lightbox preview-link"
+                                title="App 3"><i class="bx bx-plus"></i></a>
+                            <a href="portfolio-details.html" class="details-link" title="More Details"><i
+                                    class="bx bx-link"></i></a>
+                        </div>
+                    </div> --}}
+
+                    <!-- END WEBSITE -->
+
+                    <!-- ========================================================================================================================================== -->
+
+                    <!-- ADMIN -->
+
+                    {{-- <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+                        <div class="portfolio-img"><img
+                                src="{{ asset('/') }}source_Arsha/assets/img/portfolio/asset_management/Barcode.png"
+                                class="img-fluid" alt=""></div>
+                        <div class="portfolio-info">
+                            <h4>Barcode</h4>
+                            <p>Web Asset Management</p>
+                            <a href="{{ asset('/') }}source_Arsha/assets/img/portfolio/asset_management/Barcode.png"
+                                data-gallery="portfolioGallery" class="portfolio-lightbox preview-link"
+                                title="Web 2"><i class="bx bx-plus"></i></a>
+                            <a href="portfolio-details.html" class="details-link" title="More Details"><i
+                                    class="bx bx-link"></i></a>
+                        </div>
+                    </div> --}}
+
+                    {{-- <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+                        <div class="portfolio-img"><img
+                                src="{{ asset('/') }}source_Arsha/assets/img/portfolio/portfolio-2.jpg"
+                                class="img-fluid" alt=""></div>
+                        <div class="portfolio-info">
+                            <h4>Web 3</h4>
+                            <p>Web</p>
+                            <a href="{{ asset('/') }}source_Arsha/assets/img/portfolio/portfolio-2.jpg"
+                                data-gallery="portfolioGallery" class="portfolio-lightbox preview-link"
+                                title="Web 3"><i class="bx bx-plus"></i></a>
+                            <a href="portfolio-details.html" class="details-link" title="More Details"><i
+                                    class="bx bx-link"></i></a>
+                        </div>
+                    </div> --}}
+
+
+
+                    {{-- <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+                        <div class="portfolio-img"><img
+                                src="{{ asset('/') }}source_Arsha/assets/img/portfolio/portfolio-9.jpg"
+                                class="img-fluid" alt=""></div>
+                        <div class="portfolio-info">
+                            <h4>Web 3</h4>
+                            <p>Web</p>
+                            <a href="{{ asset('/') }}source_Arsha/assets/img/portfolio/portfolio-9.jpg"
+                                data-gallery="portfolioGallery" class="portfolio-lightbox preview-link"
+                                title="Web 3"><i class="bx bx-plus"></i></a>
+                            <a href="portfolio-details.html" class="details-link" title="More Details"><i
+                                    class="bx bx-link"></i></a>
+                        </div>
+                    </div> --}}
+
+                    <!-- END ADMIN -->
+
+
+                    <!-- ========================================================================================================================================== -->
+                </div>
+
+            </div>
+        </section>
+        <!-- End TEMPLATE Section -->
 
         <!-- ======= CV Section ======= -->
         <section id="cv" class="cv">
